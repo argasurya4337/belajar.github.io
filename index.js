6cac68242d6531957f78440eeb16e2bfe964fef6
argasurya4337 <suryapratama5435@gmail.com>
2023-12-20 09:02:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// 1.mencari Keliling Persegi Panjang
+// Memasukan nilai panjang, lebar
+// Menghitung rumus: 2*P + 2*L
+// Menampilkan Hasil
+
+// Memasukan nilai panjang, lebar
+let panjang = 150;
+let lebar = 40;
+
+// Menghitung Rumus
+let hasil = 2 * panjang + 2 * lebar;
+
+// Menampilkan
+console.log("Keliling persegi panjang adalah: " + hasil)
+
+// 2.Luas Lingkaran
+
+// Tentukan phi dan r
+let phi = 3.14;
+let r = 10
+
+let hasill = phi * r
+
+console.log("Hasilnya: " + hasill)
+
+let Lp = 64;
+let sisi = Math.sqrt(Lp)
+
+let hasilNya = 4 * sisi;
+
+console.log(hasilNya)
+
+// Syarat umum membuat KTP
+// input
+let umur = 13;
+
+// proses/ logika
+if(umur >= 17){
+    result = "Kolot";
+} else {
+    result = "Balita"
+}
+
+console.log(result)
+
